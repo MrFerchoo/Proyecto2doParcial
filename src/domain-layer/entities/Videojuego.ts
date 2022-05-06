e1feb1952c621d9c7bde05d1a6c096658d799392
Fernando Miramontes <fernandomiramontes03@gmail.com>
2022-05-06 14:36:28 -0500

    archivos

--- src/domain-layer/entities/Videojuego.ts
+++ src/domain-layer/entities/Videojuego.ts
@@ -1,13 +1,19 @@
-export default class Videojuego {
-  public id: string;
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
+export default class Videojuego {
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true, zerofill: true })
+  public id: number;
+
+  @Column({ type: 'varchar', length: 16, nullable: false })
   public name: string;
 
-  public year: number;
+  @Column({ type: 'varchar', length: 16, nullable: false })
+  public year: string;
 
-  constructor(id: string, name: string, year: number) {
+  public constructor(id: number, name: string, year: string) {
     this.id = id;
-    this.name = name; 
+    this.name = name;
     this.year = year;
   }
 }
