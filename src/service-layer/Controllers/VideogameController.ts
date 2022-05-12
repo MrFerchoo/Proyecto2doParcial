753089d78075716b2b4878b30bf2e3d6a3714c5c
Alexfranco64 <lfr73696@udelasalle.edu.mx>
2022-05-11 23:09:11 -0500

    videogamecontroller

--- src/service-layer/Controllers/VideogameController.ts
+++ src/service-layer/Controllers/VideogameController.ts
@@ -1,18 +1,17 @@
-import { Request, Response } from 'express';
-import AddVGTask, { AddVGData} from '../Task/AddVGTask';
-import BaseController from './BaseController';
+import { Request,Response } from "express";
+import { AddInventoryInsData } from "../tasks/AddInventoryIns";
+import BaseController from "./BaseController";
 
-export default class VideogameController extends BaseController {
-  public constructor() {
-    super('/VGs');
-  }
+export default class InventoryInsController extends BaseController{
+    public constructor(){
+        super('/inventory-ins');
+    }
+    protected configureRouter(): void {
+        this.router.post('/',this.addInventoryIns);
+    }
 
-
-
-
-
-
-
-
+    private async addInventoryIns(req:Request,res:Response):Promise<void>{
+        const addCarData =<AddInventoryInsData>req.body
+    }
 
 }
